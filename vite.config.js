d7e465b9be781ec739ca885adff4ad0ffb849ef4
Romano-Gabriele <gabriele.romano@istitutoagnelli.it>
2025-01-18 01:44:14 +0100

    - deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-export default {
-  kit: {
-    paths: {
-      base: '/FLAGUESSR/',  // Usa il nome del tuo repository su GitHub
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: 'src/app.html',  // Assicurati che questo sia il percorso corretto
     },
   },
-};
+  base: '/FLAGUESSR/', // Impostazione base per GitHub Pages
+});
