8ff71e0906bea698ee8b3eee11d945b5d3a9b244
Rossi-Tommaso <tommasoemanuele.rossi@gmail.com>
2025-05-09 11:50:17 +0200

    a

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'docs',  // La cartella dove verranno salvate le pagine
+      assets: 'docs', // La cartella per gli asset
+      fallback: null,
+      precompress: false,
+      strict: true,
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : '/FLAGUESSR',  // Lasciamo la base vuota se il sito è direttamente nella root
+    },
+  },
 };
 
 export default config;
