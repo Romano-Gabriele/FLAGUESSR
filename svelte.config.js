a9c2d908658e619fd0b6708dea965c296aa6d22a
Rossi-Tommaso <tommasoemanuele.rossi@gmail.com>
2025-05-09 11:21:55 +0200

    progetto migrato in svelte 5

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      pages: 'docs',  // La cartella dove verranno salvate le pagine
-      assets: 'docs', // La cartella per gli asset
-      fallback: null,
-      precompress: false,
-      strict: true,
-    }),
-    paths: {
-      base: process.argv.includes('dev') ? '' : '/FLAGUESSR',  // Lasciamo la base vuota se il sito è direttamente nella root
-    },
-  },
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
 
 export default config;
